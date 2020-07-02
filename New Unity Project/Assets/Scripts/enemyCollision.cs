94f62ad24e1a27f6c53f9775decf030d0b7a8fb8
Mike DeAnda <mdeanda@depaul.edu>
2020-07-02 15:33:22 -0500

    updated the movement on enemy projectiles, killboxes and respawn points.

--- New Unity Project/Assets/Scripts/enemyCollision.cs
+++ New Unity Project/Assets/Scripts/enemyCollision.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class enemyCollision : MonoBehaviour
 {
+    //serializeField allows you to assign something to this in Unity
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
+
+    //this script kills the player when running into a block
     private void OnCollisionEnter(Collision collisionInfo)
     {
+        //first check if the box has run into a player
         if (collisionInfo.collider.tag == "Player")
         {
+            //if the box runs into a plyer, respawn the player
             player.transform.position = respawnPoint.transform.position;
             Physics.SyncTransforms();
+            //destroy the block
+            Destroy(gameObject);
         }
     }
+   
 }
