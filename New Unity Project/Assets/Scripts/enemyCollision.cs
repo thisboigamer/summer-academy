bd30e7c1cb86f04d4902254fe08be2b7f4687b32
Mike DeAnda <mdeanda@depaul.edu>
2020-07-02 13:40:52 -0500

    began adding an obstacle.

---
+++ New Unity Project/Assets/Scripts/enemyCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyCollision : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Player")
+        {
+            player.transform.position = respawnPoint.transform.position;
+            Physics.SyncTransforms();
+        }
+    }
+}
